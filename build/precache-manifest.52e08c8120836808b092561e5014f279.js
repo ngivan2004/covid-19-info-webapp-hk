0423c3dd63f77b1d0af900553b32f37196edf831
ivanng2014 <61515871+ivanng2014@users.noreply.github.com>
2020-03-18 23:03:56 +0800

    buildfinna

---
+++ build/precache-manifest.52e08c8120836808b092561e5014f279.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7105a60c3e99c475da4c00662547b082",
+    "url": "/index.html"
+  },
+  {
+    "revision": "91a9384ab371df4cd179",
+    "url": "/static/css/main.b9e782dd.chunk.css"
+  },
+  {
+    "revision": "4cdc9f2b76802a99d852",
+    "url": "/static/js/2.532f216e.chunk.js"
+  },
+  {
+    "revision": "496964241257fb8557b021595212d9cf",
+    "url": "/static/js/2.532f216e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91a9384ab371df4cd179",
+    "url": "/static/js/main.8c4daa75.chunk.js"
+  },
+  {
+    "revision": "78e344cfe8d7e1bf8338",
+    "url": "/static/js/runtime-main.3fd3e335.js"
+  }
+]);
